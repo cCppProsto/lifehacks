7b8c9510f38db87051adc9e14331250104fccbd8
ioleshko <ioleshko@ioleshko>
2018-02-05 11:09:55 +0200

    was added convert video to gif

---
+++ video_to_gif/create_gif.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [[ -z $1 ]] ; then
+    echo 'input video file name(full path)'
+    exit 1
+fi
+
+if [[ -z $2 ]] ; then
+    echo 'input output gif file name'
+    exit 1
+fi
+
+
+skip_seconds=30 # skip first N second
+output_lenght=6 # output lenght N second
+fps=5           # N frames per second
+image_width=640 # N pixels wide and automatically determine the height while preserving the aspect ratio
+
+ffmpeg -y -ss $skip_seconds -t $output_lenght -i "$1" -vf fps=$fps,scale=$image_width:-1:flags=lanczos,palettegen palette.png
+ffmpeg -ss    $skip_seconds -t $output_lenght -i "$1" -i palette.png -filter_complex "fps=$fps,scale=$image_width:-1:flags=lanczos[x];[x][1:v]paletteuse" "$2"
+
+rm palette.png
