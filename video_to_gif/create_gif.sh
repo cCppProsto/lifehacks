74a1fee01b3ce7a972fb59dc90a7a9eee2148fd8
ioleshko <ioleshko@ioleshko>
2018-05-08 11:07:53 +0300

    upd

--- video_to_gif/create_gif.sh
+++ video_to_gif/create_gif.sh
@@ -13,8 +13,8 @@ fi
 
 skip_seconds=30 # skip first N second
 output_lenght=6 # output lenght N second
-fps=5           # N frames per second
-image_width=640 # N pixels wide and automatically determine the height while preserving the aspect ratio
+fps=10          # N frames per second
+image_width=320 #N pixels wide and automatically determine the height while preserving the aspect ratio
 
 ffmpeg -y -ss $skip_seconds -t $output_lenght -i "$1" -vf fps=$fps,scale=$image_width:-1:flags=lanczos,palettegen palette.png
 ffmpeg -ss    $skip_seconds -t $output_lenght -i "$1" -i palette.png -filter_complex "fps=$fps,scale=$image_width:-1:flags=lanczos[x];[x][1:v]paletteuse" "$2"
